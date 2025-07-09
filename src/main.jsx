7618c035383f015d0fc43cce0ff15c8e29f9afce
junglesub <junglesubmarine@gmail.com>
2025-07-09 13:23:11 +0900

    cap

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import Survey from "./pages/survey.jsx";
+import Survey from "./pages/Survey.jsx";
 import SurveyResult from "./pages/SurveyResult.jsx";
 import LandPage from "./pages/LandPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
