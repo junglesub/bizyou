feca12ceb11b2840bc42ee129bad8a427cc4cf6c
junglesub <junglesubmarine@gmail.com>
2025-07-09 13:22:26 +0900

    cap change

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import Home from "./pages/home.jsx";
 import Survey from "./pages/survey.jsx";
 import SurveyResult from "./pages/SurveyResult.jsx";
 import LandPage from "./pages/LandPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
+import Home from "./pages/Home.jsx";
 
 const router = createBrowserRouter([
   {
